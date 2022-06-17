d4a01c70c3a0e285c0f406b9ec2e739269ac49f6
Gridy <reality023@naver.com>
2022-06-17 16:24:08 +0900

    Reset 파일 및 GlobalStyle 적용

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import Router from "./Router";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+import { defaultTheme } from "./shared/theme";
+import reset from "./shared/reset.css";
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
       <Router />
-    </>
+    </ThemeProvider>
   );
 }
+
+const GlobalStyle = createGlobalStyle`
+${reset}; // Reset CSS
+
+body {
+  color: #000000;
+}
+
+body, button, input, textarea {
+  color: #000000;
+  font-family: ${(props) => props.theme.fontFamily.default}, sans-serif;
+}
+
+a {
+  text-decoration: none;
+  color: inherit;
+}
+`;
 
 export default App;
