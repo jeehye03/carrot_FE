566a7440a91bada40c743ec4b049eefa831b80e6
epppo <amorosso@kakao.com>
2022-06-18 10:35:48 +0900

    main basic design 완성

--- src/components/MainItemList.js
+++ src/components/MainItemList.js
@@ -1,38 +1,54 @@
 import '../shared/listForm.css';
 import styled from "styled-components";
-import { AiFillHome } from 'react-icons/ai';
-import { BiUser } from 'react-icons/bi';
-import { MdArrowForwardIos } from 'react-icons/md'
-import {AiFillHeart} from 'react-icons/ai'
-import { BsFillBasket2Fill } from 'react-icons/bs'
-import { BsReceipt } from 'react-icons/bs'
+import { AiFillPlusCircle } from 'react-icons/ai';
 
-
-import { useNavigate } from "react-router-dom";
-
-function MyPage () {
+function MainItemList () {
   return ( 
       <div className="ContentsBox"> 
-        <div className='card'>
-          <img src="https://velog.velcdn.com/images/eppo/post/2e1a14ff-f6cd-4f63-9cd1-330722065e62/image.png"/>
+        <CardBox className='card'>
+          <div style={{display:'flex'}}>
+            <Img src="https://velog.velcdn.com/images/eppo/post/2e1a14ff-f6cd-4f63-9cd1-330722065e62/image.png"/>
+            <TextArea>
+                <span>귀엽고 깜찍한 춘식이 팔아요</span>
+                <span>군자동</span>
+                <span>30,000원</span>
+            </TextArea>
         </div>
-      
+        <div style={{display:'flex', alignItems: 'flex-end'}}>❤️</div>
+        </CardBox>
+        
+        <FixedButton>
+          <AiFillPlusCircle size="60" color='#ff7E36'/>
+        </FixedButton>
       </div>
     )
 
 }
 
-const MyInfoBox = styled.div`
-display: flex;
+
+const CardBox = styled.div`
+display:flex;
+padding: 15px;
 justify-content: space-around;
-border-bottom: 1px solid #AAAAAA;
-height: 150px;
-}`;
+border-bottom: 1px solid  #AAAAAA;
+`;
 
 
+const TextArea = styled.div`
+display: flex;
+flex-direction: column;
+padding: 10px
+`;
+
+const FixedButton = styled.div`
+display: flex;
+position: fixed;
+bottom: 13%;
+right: 5%;
+`;
 
 
-const Img = styled.img` width:100px`;
+const Img = styled.img` width:100px; border-radius: 10px; `;
 
 
-export default MyPage;
+export default MainItemList;
