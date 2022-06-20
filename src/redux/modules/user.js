b6f5de1d95f96fae7280fa67b9b683860be0d787
Gridy <reality023@naver.com>
2022-06-20 15:53:07 +0900

    유저 정보 로드 작업 #8

--- src/redux/modules/user.js
+++ src/redux/modules/user.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { loadProfile } from "../../shared/axios";
 
 export const carrotLoginStatus = (status) => {
   return async function (dispatch) {
@@ -6,21 +7,33 @@ export const carrotLoginStatus = (status) => {
   }
 };
 
+export const getCarrotUserInfo = () => {
+  return async function (dispatch) {
+    const response = await loadProfile();
+    dispatch(setUser(response.data.user));
+  }
+}
+
 //Reducer
 const userSlice = createSlice({
   name: "user",
   initialState: {
     isLogin: false,
-    profileImage: "",
     nickname: "",
-    location: ""
+    userLocation: "",
+    userImg: "",
   },
   reducers: {
+    setUser: (state, action) => {
+      state.nickname = action.payload.nickname;
+      state.userLocation = action.payload.userLocation;
+      state.userImg = action.payload.userImg;
+    },
     updateLogin: (state, action) => {
       state.isLogin = action.payload;
     },
   },
 });
 
-const { updateLogin } = userSlice.actions;
+const { updateLogin, setUser } = userSlice.actions;
 export default userSlice.reducer;
--- src/shared/axios.js
+++ src/shared/axios.js
@@ -10,3 +10,4 @@ export const instance = axios.create({
 export const login = async (users) => await instance.post("/api/user/login", users);
 export const register = async (users) => await instance.post("/api/user/signup", users);
 export const editProfile = async (userImg, nickname, userLocation) => await instance.put("/api/user/edit", {userImg, nickname, userLocation});
+export const loadProfile = async () => await instance.get("/api/user/me");
