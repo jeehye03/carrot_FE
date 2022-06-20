cb56100277669834e567b26091c20164af946a3b
jeehye03 <ks6247@naver.com>
2022-06-20 13:56:36 +0900

    충돌 해결!

--- src/redux/modules/post.js
+++ src/redux/modules/post.js
@@ -35,30 +35,29 @@ export const carrotGetPost = () => {
 // 메인화면 포스트
 export const loadMainposts = () => {
   return async function (dispatch) {
-
-    await axios.get("http://54.180.121.151/api/post").then((re) => {
-      dispatch(roadPosts(re.data));
-    });
+    await axios
+      .get("http://54.180.121.151/api/post")
+      .then((re) => {
+        dispatch(roadPosts(re.data));
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
 };
 
-      await axios.get("http://54.180.121.151/api/post").then(re=>{
-          dispatch(roadPosts(re.data));
-      }).catch((err) => { console.log(err);});
-  }
-}
-
-export const loadSalseposts = () =>{
+export const loadSalseposts = () => {
   return async function (dispatch) {
-      await axios.get("http://54.180.121.151/api/user").then(re=>{
-          dispatch(roadPosts(re.data));
-      }).catch((err) => {console.log(err);});
-  }
-}
-
-
-
-
+    await axios
+      .get("http://54.180.121.151/api/user")
+      .then((re) => {
+        dispatch(roadPosts(re.data));
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+};
 
 //Reducer
 const postSlice = createSlice({
