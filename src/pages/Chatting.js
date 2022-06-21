e279276da150969a861a285335f8c43acd9028a3
epppo <amorosso@kakao.com>
2022-06-21 22:53:33 +0900

    채팅 컨포넌트 수정 / 소켓 Import

--- src/pages/Chatting.js
+++ src/pages/Chatting.js
@@ -1,24 +1,62 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+
 import {MdPlayArrow} from "react-icons/md";
 import {FaCalendarCheck} from "react-icons/fa";
-
-import { useDispatch, useSelector } from "react-redux";
+import { BiLeftArrowAlt } from "react-icons/bi";
+import { useSelector } from "react-redux";
 
 import styled from "styled-components";
 
+import io from 'socket.io-client';
+const socket =  io.connect('http://localhost:4000')
+
 function Chatting(){
-    const params = useParams();
-    const postId = params.postid;
+    const [state, setState] = useState({message:'', name:''})
+    const [chat,setChat] =useState([])
+
     const user = useSelector((state) => state.user);
     const postDetail = useSelector((state) => state.post.post.detailPost);
 
+    const params = useParams();
+    const postId = params.postid;
+    const userName = user.nickname;
+    const navigate = useNavigate();
+
+
+    // useEffect(() => {
+    //     socket.on('message', ({ userName, message }) => {
+    //         setChat([...chat, { userName, message }])
+    //     })
+    // }, [])
+
+    // const onTextChange = e => {
+    //     setState({ ...state, [e.target.name]: e.target.value })
+    // }
+
+    // const onMessageSubmit = (e) => {
+    //     e.preventDefault()
+    //     const { name, message } = state
+    //     socket.emit('message', { name, message })
+    //     setState({ message: '', name })
+    // }
+
+    // const renderChat = () => {
+    //     return chat.map(({ name, message }, index) => (
+    //         <div key={index}>
+    //             <h3>{name}:<span>{message}</span></h3>
+    //         </div>))
+    // }
 
     
     return (
         <>
-            <HeaderDesign>{user.nickname}</HeaderDesign>
+            <Header>
+                <div> <BiLeftArrowAlt size="25" onClick={() =>  navigate(-1)} /></div>
+                <div> <p>{user.nickname}</p> </div>
+            </Header>
             
+            <div style={{padding:'8px'}}>
             <ProductInfo>
                 <Img src={postDetail.postImg}/>
                 <div>                
@@ -29,10 +67,19 @@ function Chatting(){
                 <BtnStyle><FaCalendarCheck size="15" style={{marginRight:"5px"}}/>예약하기</BtnStyle>
             </ProductInfo>
             
-            <ChatBody>채팅바디</ChatBody>
-            
+            <ChatBody>
+                <div style={{display:'flex', alignItems:'center', justifyContent:'flex-end'}}>
+                <MyTextArea>네고 안되나요?</MyTextArea> <br/></div>
+
+                <div style={{display:'flex', alignItems:'center', justifyContent:'flex-start'}}>
+                    <img style={{width:'50px'}}src={user.userImg}/>
+                    <ReceiveTextArea>안돼요</ReceiveTextArea>
+                </div>
+                
+            </ChatBody>
+            </div>
             <FootterMenu>
-                <ChattingInput></ChattingInput>
+                <ChattingInput/>
                 <MdPlayArrow size="40" color="darkgray"/>
             </FootterMenu>
         </>
@@ -41,17 +88,47 @@ function Chatting(){
 export default Chatting;
 
 
+const MyTextArea = styled.div`
+background-color: #FF7E36;
+height: 30px;
+border-radius: 20px;
+color: white;
+font-weight: bold;
+font-size: 15px;
+display: inline-block;
+width: auto;
+padding: 8px;
+`;
 
-const HeaderDesign = styled.div`
-height: 50px;
-border-bottom: 1px solid #AAAAAA;
-display: flex;
-align-items: center;
-justify-content: center;
+const ReceiveTextArea = styled.div`
+background-color: gray;
+height: 30px;
+border-radius: 20px;
+color: white;
+font-weight: bold;
+font-size: 15px;
+display: inline-block;
+width: auto;
+padding: 8px;
+`;
+
+
+const Header = styled.header`
+  display: flex;
+  align-items: center;
+  padding: 16px 15px;
+  border-bottom: 1px solid #dadada;
+
+  & div:last-child {
+    width: 85%;
+    text-align: center;
+  }
 `;
 
 const ChatBody = styled.div`
-height: 645px;
+height: 610px;
+margin-top: 20px;
+overflow: scroll;
 `;
 
 const ProductInfo = styled.div`
@@ -78,7 +155,7 @@ align-items: center;
 const Img = styled.img` width: 80px; padding: 5px;`;
 
 const FootterMenu = styled.div`
-display: flex;
+display:flex;
 background-color: lightgray;
 height: 50px;
 align-items: center;
