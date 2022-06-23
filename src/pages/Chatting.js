c5dcde1d7100b187a7d3f4d42d45f4d3571b1ad7
epppo <amorosso@kakao.com>
2022-06-23 13:35:14 +0900

    Sent 메세지 오른쪽으로 정렬 자동 스크롤바 적용 글씨 길이만큼 출력 가격 따옴표 예약중 토글 상품 이미지 적용

--- src/pages/Chatting.js
+++ src/pages/Chatting.js
@@ -1,8 +1,11 @@
 import "../public/css/chatting.css"
+import styled from "styled-components";
 
 import React, { useState, useEffect, useRef } from "react";
-import { useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import { changeTradeStateDB } from "../redux/modules/post";
+
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate, useParams } from "react-router-dom";
 
 import { AiOutlineArrowLeft } from "react-icons/ai";
 import {AiOutlineCaretRight} from "react-icons/ai";
@@ -14,14 +17,19 @@ const socket = io.connect('http://54.180.121.151',{
   cors :{origin:'http://54.180.121.151'}
 });
 
-//const socket = io.connect('http://localhost:4000');
+// const socket = io.connect('http://localhost:4000');
 
 function Chatting(){
+  const dispatch = useDispatch();
   const user = useSelector((state) => state.user); // 유저 정보
   const navigate = useNavigate();
   const nickname = user.nickname;
   const userImg = user.userImg;
   const messageRef = useRef();
+  const scrollRef = useRef();
+  const params = useParams();
+  const state = params.trade;
+  const [sellState,setSellState] =useState(state)
 
   const [chat,setChat] =useState([])  // 데이터가 실시간으로 쌓여서 출력하는 스테이트 받아서 뿌리는 건 쳇이야 
   const sellPInfo = useSelector((state) => state.post.post);
@@ -35,9 +43,13 @@ function Chatting(){
         userImg: userImg,
         time : time
       }
-      setChat((chat)=> chat.concat(addChat));   
+      setChat((chat)=> chat.concat(addChat));  
     })
   },[])
+
+  useEffect(()=>{
+    scrollRef.current.scrollTo(0, scrollRef.current.scrollHeight); 
+  },[chat]);
 
 
   const cleanInput = () => {let input = document.getElementById('inputBox'); input.value="";}
@@ -66,22 +78,29 @@ return (
       </div>
       
        <div className="saleList">
-        <img src={user.userImg}/>
-        <p style={{fontWeight:'bold'}}>판매중</p>
+        <img src={sellPInfo.postImg}/>
         <span className="productInfo">
           
           <p>{sellPInfo.title}</p>
-          <p>{sellPInfo.price}원</p>
+          <p>{Number(sellPInfo.price).toLocaleString("ko-KR")}원</p>
+          <span> {sellState === "1" ? ( <Book>예약중</Book>) : sellState === "2" ? ( <SoldOut>거래완료</SoldOut> ) 
+                                : ( "" )} </span>
         </span>
-        <button>예약하기</button>
+        {sellState === "1" ? (  <button disabled >예약중</button>) : sellState === "2" ? ( <button disabled>거래완료</button> ) 
+         : sellState === "0" ? ( <button style={{background : '#FF7E36', color:'white'}} onClick={()=>{
+          dispatch(changeTradeStateDB(sellPInfo.postId, "1"))
+          alert("예약이 완료되었습니다. ")
+          setSellState("1");}}>예약하기</button>) : ""}
+  
+        
       </div>
     
-      <div className="chattingBody"> 
+      <div className="chattingBody" ref={scrollRef}>
       {chat && chat.map((list, index) => (
-        <>
+        <div key={index}>
           {list.nickname === user.nickname ?
 
-            <div className="chattingList" style={{flexDirection : 'row-reverse'}}>
+            <div className="chattingList" style={{flexDirection : 'row-reverse', float:'right'}}>
               <span className="profile">
                 <span className="user">{list.nickname}</span>
                 <img className="img" src={list.userImg} style={list.style} />
@@ -105,9 +124,8 @@ return (
 
             </div>
           }
-  
-        </>
-      ))}
+          </div>
+        ))}
 
       
       </div>
@@ -125,5 +143,22 @@ return (
   );
 
 }
+
+const SoldOut = styled.div`
+  
+  padding: 6px 5px;
+  width: 65px;
+  border-radius: 5px;
+  background-color: #565656;
+  color: white;
+  font-size: 12px;
+  text-align: center;
+`;
+
+const Book = styled(SoldOut)`
+  width: 55px;
+  background-color: #34bf9e;
+`;
+
 
 export default Chatting;
